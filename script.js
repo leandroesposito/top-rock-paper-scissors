39f2c4b4ad071656ece346debfecc140ba2f3055
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2024-12-29 18:40:24 -0300

    Implement getHumanChoice() function

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ function getComputerChoice() {
 
     return choice;
 }
+
+function getHumanChoice() {
+    let isValidChoice = false;
+    let choice = null;
+    // while choice is invalid
+    while (!isValidChoice) {
+        // get choice from user using prompt
+        choice = prompt('Write your choice between "rock", "paper" and "scissors".');
+        // check if choice is null
+        if (choice) {
+            // trim choice
+            // convert to lowercase
+            choice = choice.trim().toLowerCase();
+            // validate choice
+            isValidChoice = choice === "rock" || choice === "paper" || choice === "scissors";
+        }
+    }
+
+    // return choice
+    return choice;
+}
