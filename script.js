9e3e9849392fb651f0a54f99d7909b1d131b16a7
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2025-01-02 18:38:45 -0300

    Add eventListener to buttons
    Play a round when user click any button
    Log selections and results to console for now

--- script.js
+++ script.js
@@ -20,72 +20,85 @@ function getHumanChoice() {
     return choice;
 }
 
-function playGame(numRounds) {
-    function playRound(humanChoice, computerChoice) {
-        let message = "";
-        if (humanChoice === "rock") {
-            if (computerChoice === "rock") {
-                message = "Draw, both chose Rock.";
-            }
-            else if (computerChoice === "paper") {
-                message = "You lose! Paper beats Rock";
-                computerScore++;
-            }
-            else if (computerChoice === "scissors") {
-                message = "You win! Rock beats Scissors";
-                humanScore++;
-            }
-        }
-        else if (humanChoice === "paper") {
-            if (computerChoice === "rock") {
-                message = "You win! Paper beats Rock";
-                humanScore++;
-            }
-            else if (computerChoice === "paper") {
-                message = "Draw, both chose Paper.";
-            }
-            else if (computerChoice === "scissors") {
-                message = "You lose! Scissors beats Paper";
-                computerScore++;
-            }
-        }
-        else if (humanChoice === "scissors") {
-            if (computerChoice === "rock") {
-                message = "You lose! Rock beats Scissors";
-                computerScore++;
-            }
-            else if (computerChoice === "paper") {
-                message = "You win! Scissors beats Paper";
-                humanScore++;
-            }
-            else if (computerChoice === "scissors") {
-                message = "Draw, both chose Scissors.";
-            }
-        }
-        console.log(message);
-    }
 
-    function showResults(humanScore, computerScore) {
-        console.log(`Results | You: ${humanScore} | Computer: ${computerScore}`);
-        if (humanScore > computerScore) {
-            console.log("You win!");
+function playRound(humanChoice, computerChoice) {
+    let message = "";
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            message = "Draw, both chose Rock.";
         }
-        else if (humanScore < computerScore) {
-            console.log("You lose!");
+        else if (computerChoice === "paper") {
+            message = "You lose! Paper beats Rock";
+            computerScore++;
         }
-        else {
-            console.log("Draw");
+        else if (computerChoice === "scissors") {
+            message = "You win! Rock beats Scissors";
+            humanScore++;
         }
     }
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < numRounds; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+    else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            message = "You win! Paper beats Rock";
+            humanScore++;
+        }
+        else if (computerChoice === "paper") {
+            message = "Draw, both chose Paper.";
+        }
+        else if (computerChoice === "scissors") {
+            message = "You lose! Scissors beats Paper";
+            computerScore++;
+        }
     }
-
-    showResults(humanScore, computerScore);
+    else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            message = "You lose! Rock beats Scissors";
+            computerScore++;
+        }
+        else if (computerChoice === "paper") {
+            message = "You win! Scissors beats Paper";
+            humanScore++;
+        }
+        else if (computerChoice === "scissors") {
+            message = "Draw, both chose Scissors.";
+        }
+    }
+    console.log(message);
 }
+
+function showResults(humanScore, computerScore) {
+    console.log(`Results | You: ${humanScore} | Computer: ${computerScore}`);
+    if (humanScore > computerScore) {
+        console.log("You win!");
+    }
+    else if (humanScore < computerScore) {
+        console.log("You lose!");
+    }
+    else {
+        console.log("Draw");
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const elementsContainer = document.querySelector(".elements-container");
+
+elementsContainer.addEventListener("click", (event) => {
+    if (event.target.id === "") {
+        return;
+    }
+
+    playRound(event.target.id, getComputerChoice());
+    showResults(humanScore, computerScore);
+});
+
+
+/*
+TODO
+
+Add a div for displaying results and change all of your console.logs into DOM methods.
+
+Display the running score, and announce a winner of the game once one player reaches 5 points.
+
+You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
+*/
